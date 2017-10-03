31312f64988f736bea7a5cb37a7c94d666ccbe17
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2017-10-03 09:29:01 +0100

    Add a bit more checking and helpfulness to backfill script

--- backfill.py
+++ backfill.py
@@ -7,11 +7,15 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 parser = argparse.ArgumentParser(description='Backfill data in pay-performance-platform')
-parser.add_argument('--context', required=True)
-parser.add_argument('--start', required=True)
-parser.add_argument('--end', required=True)
+parser.add_argument('--context', required=True, help="Either 'test' to send data to test endpoint, or 'payments' to send to real endpoint")
+parser.add_argument('--start', required=True, help='Inclusive start date in format Y-m-d')
+parser.add_argument('--end', required=True, help='Inclusive end date in format Y-m-d')
 
 args = parser.parse_args()
+
+if args.context not in ['test', 'payments']:
+	raise Exception('context must be either "test" or "payments"')
+
 print 'Backfilling with context ' + args.context + ' for dates ' + args.start + ' to ' + args.end
 
 start = datetime.strptime(args.start + ' 23:59:59', "%Y-%m-%d %H:%M:%S")
@@ -20,4 +24,3 @@ end = datetime.strptime(args.end + ' 23:59:59', "%Y-%m-%d %H:%M:%S")
 for date in daterange(start, end):
 	print 'Backfilling ' + date.isoformat()
 	ppp.lambda_handler({'time': date.isoformat()}, args.context)
-
